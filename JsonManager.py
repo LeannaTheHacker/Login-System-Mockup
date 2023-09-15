122322725cec12d6b95a59e14bd9274c2523e0a0
LeannaTheHacker <136647143+LeannaTheHacker@users.noreply.github.com>
2023-09-15 18:28:34 -0400

    Adding base for code

---
+++ JsonManager.py
@@ -0,0 +1,16 @@
+class JsonManager:
+    # TODO addUser
+    # TODO deleteUser
+    # TODO updateLoginStatus
+
+    def __init__(this, jsonFilePath: str):
+        this.jsonFilePath: str = jsonFilePath
+
+    def addUser(this, name: str, subteam: str, grade: str):
+        pass
+
+    def deleteUser(this, name: str):
+        pass
+
+    def updateLoginStatus(this, name: str, timeAndDate: str):
+        pass
---
+++ main.py
@@ -0,0 +1,16 @@
+
+from JsonManager import JsonManager
+
+
+def main():
+    print("Running Test Login System")
+
+    jsonFilePath: str = ".\\Users.json"
+    jsonManager: JsonManager = JsonManager(jsonFilePath)
+
+    # TODO Adding new user
+    # TODO Deleting user
+    # TODO Login user 
+
+if __name__ == "__main__":
+    main()
